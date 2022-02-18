73e9d9b1bf587ed2eee9e1c610e5f72a773bbea1
Huzeir Kurpejovich <zorrobeats@Huzeirs-MacBook-Air.local>
2022-02-17 19:28:56 -0500

    finished the batch processing application

---
+++ src/main/java/io/github/huzeir2k/springbatchpractice/SpringBatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package io.github.huzeir2k.springbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeApplication {
+
+	public static void main(String[] args) throws Exception {
+		System.exit(SpringApplication.exit(SpringApplication.run(SpringBatchPracticeApplication.class, args)));
+	}
+
+}
---
+++ src/test/java/io/github/huzeir2k/springbatchpractice/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.huzeir2k.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
